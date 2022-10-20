d6c80670b4e7910372dad20cf1f5f3b343bd1670
KoshmareG <koshmareg@yandex.ru>
2022-10-20 20:46:50 +0700

    Changed suit names to icons

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,4 @@
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+suits = ["\u2666", "\u2665", "\u2663", "\u2660"] #Diamonds, Hearts, Clubs, Spades
 
 puts "#{values.sample} #{suits.sample}"
